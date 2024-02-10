cf6b7abf1b519d74ab1250d76693bbfb225ce95b
dippal513 <dip.pal.513@gmail.com>
2024-02-10 09:22:47 +0600

    url changed to production

--- src/lib/AxiosInstance.js
+++ src/lib/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://truthgate-backend.vercel.app",
   withCredentials: true
 });
 
